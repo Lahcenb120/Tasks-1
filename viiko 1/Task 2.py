475c1518bda88a2846e39317cb252ad7ea3f4580
lahcen <lahcenbenbraim@gmail.com>
2026-02-04 11:56:15 +0200

    tasks

---
+++ viiko 1/Task 2.py
@@ -0,0 +1,9 @@
+nimi = input("give your name: ")
+print ("Hello " + nimi)
+print("nice to meet you " + nimi + ":D")
+
+print(f"hello, {nimi}")
+
+
+nimi2 = "Ben"
+
--- viiko 1/task1.py
+++ viiko 1/task1.py
@@ -1,2 +1,27 @@
-print("hello, world")
-print("hello, world")
+import math
+
+nimi = input("give your name: ")
+
+print(f"hello, {nimi}")
+
+nimi2 = "Ben"
+
+print(f"hello, {nimi}")
+
+
+ika = int(input("give your age:"))
+lisa = 10
+
+print("käyttäjän ikä on:", ika + lisa)
+
+ika = float(input("give your age:"))
+lisa = 10
+
+print("käyttäjän ikä on:", ika + lisa)
+
+pituus = float(input("give your hight:"))
+paino = float(input("give your weight:"))
+
+bmi = paino / (pituus / 100)**2
+
+print("your BMI is:", bmi)
